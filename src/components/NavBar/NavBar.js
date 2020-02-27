734a6dda70f4faf335ef2d87c0bd909419e9f71f
Rashmi-Shree01 <50406213+Rashmi-Shree01@users.noreply.github.com>
2020-02-27 17:59:29 +0530

    hi

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,59 @@
 import React from 'react'; 
-import {NavLink} from 'react-router-dom';
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap'; 
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, ButtonGroup} from 'react-bootstrap'; 
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-       <Navbar bg="dark" expands="lg">
-           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-           <Navbar.Collapse id="basic-navbar-nav">
-               <Nav>
-                   <NavLink className="d-inline p-2 bg-dark text-white"
-                   to="/">Home</NavLink>
-                   <NavLink className="d-inline p-2 bg-dark text-white"
-                   to="/department">Department</NavLink>
-                   <NavLink className="d-inline p-2 bg-dark text-white"
-                   to="/employee">Employee</NavLink>
-               </Nav>
-           </Navbar.Collapse>
-       </Navbar>
+        <Navbar bg="light" expand="lg" className="hii">
+        <Navbar.Brand href="/home">Immovables</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Buy" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Rent" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Sell" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Tools&Advices" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Help" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+          <ButtonGroup aria-label="Basic example">
+            <Button variant="secondary">SignIn</Button>
+        </ButtonGroup>
+        </Navbar.Collapse>
+      </Navbar>
     );
 }
 
