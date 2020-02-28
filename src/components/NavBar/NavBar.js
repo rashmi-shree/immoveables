135f4595c9e6dcdba5a9ac76de15bfb11377ef0d
Rashmi-Shree01 <50406213+Rashmi-Shree01@users.noreply.github.com>
2020-02-28 16:44:26 +0530

    hi

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,59 +1,61 @@
 import React from 'react'; 
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button, ButtonGroup} from 'react-bootstrap'; 
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'; 
 import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg" className="hii">
-        <Navbar.Brand href="/home">Immovables</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="Buy" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+      <Navbar collapseOnSelect expand="lg" bg="dark" className="nb">
+      <Navbar.Brand id='logo' href="/home">Immovables</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+        <NavDropdown title={<span className="check">Buy</span>} id="basic-nav-dropdown">
+              <NavDropdown.Item href="/flats">Flats</NavDropdown.Item>
+              <NavDropdown.Item href="/land">Land</NavDropdown.Item>
+              <NavDropdown.Item href="/bungalows">Bungalows</NavDropdown.Item>
+              <NavDropdown.Item href="/apartments">Apartments</NavDropdown.Item>
+              <NavDropdown.Item href="/independent_house">Independent House</NavDropdown.Item>
+              <NavDropdown.Item href="/out_house">Out House</NavDropdown.Item>
+              <NavDropdown.Item href="/pent_house">Pent House</NavDropdown.Item>
+              {/* <NavDropdown.Divider />
+              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item> */}
             </NavDropdown>
-            <NavDropdown title="Rent" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            <NavDropdown title={<span className="check">Rent</span>} id="basic-nav-dropdown">
+            <NavDropdown.Item href="/flats">Flats</NavDropdown.Item>
+              <NavDropdown.Item href="/land">Land</NavDropdown.Item>
+              <NavDropdown.Item href="/bungalows">Bungalows</NavDropdown.Item>
+              <NavDropdown.Item href="/apartments">Apartments</NavDropdown.Item>
+              <NavDropdown.Item href="/independent_house">Independent House</NavDropdown.Item>
+              <NavDropdown.Item href="/out_house">Out House</NavDropdown.Item>
+              <NavDropdown.Item href="/pent_house">Pent House</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Sell" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            <NavDropdown title={<span className="check">Sell</span>} id="basic-nav-dropdown">
+            <NavDropdown.Item href="/land">Land</NavDropdown.Item>
+              <NavDropdown.Item href="/bungalows">Bungalows</NavDropdown.Item>
+              <NavDropdown.Item href="/apartments">Apartments</NavDropdown.Item>
+              <NavDropdown.Item href="/independent_house">Independent House</NavDropdown.Item>
+              <NavDropdown.Item href="/out_house">Out House</NavDropdown.Item>
+              <NavDropdown.Item href="/pent_house">Pent House</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Tools&Advices" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            <NavDropdown title={<span className="check">Lease</span>} id="basic-nav-dropdown">
+            <NavDropdown.Item href="/land">Land</NavDropdown.Item>
+              <NavDropdown.Item href="/bungalows">Bungalows</NavDropdown.Item>
+              <NavDropdown.Item href="/apartments">Apartments</NavDropdown.Item>
+              <NavDropdown.Item href="/independent_house">Independent House</NavDropdown.Item>
+              <NavDropdown.Item href="/out_house">Out House</NavDropdown.Item>
+              <NavDropdown.Item href="/pent_house">Pent House</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Help" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/action">Action</NavDropdown.Item>
-              <NavDropdown.Item href="/anotheraction">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="/something">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/seperatedlink">Separated link</NavDropdown.Item>
+            <NavDropdown title={<span className="check">Help</span>} id="basic-nav-dropdown">
+              <NavDropdown.Item href="/helpcenter">Help Center</NavDropdown.Item>
+              <NavDropdown.Item href="/salesenquiry">Sales Enquiry</NavDropdown.Item>
             </NavDropdown>
-          </Nav>
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form> */}
-          <ButtonGroup aria-label="Basic example">
-            <Button variant="secondary">SignIn</Button>
-        </ButtonGroup>
-        </Navbar.Collapse>
-      </Navbar>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/signup" id="su">Sign Up</Nav.Link>
+          <Nav.Link href="/login" className="l">Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     );
 }
 
---
+++ src/components/SubNavBar/SubNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap'; 
+import './SubNavBar.css';
+
+const SubNavBar = () => {
+    return(
+        <Navbar collapseOnSelect expand="lg" className="nb1">Hi</Navbar>
+    );
+}
+
+export default SubNavBar;
+
+// search bar: https://mdbootstrap.com/docs/react/forms/search/?
